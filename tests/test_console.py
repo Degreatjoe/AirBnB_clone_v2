2fed23849348105328111532e554966232eb2053
Great~ <jgreat779@gmail.com>
2024-06-24 13:51:22 +0100

    updates

--- tests/test_console.py
+++ tests/test_console.py
@@ -5,13 +5,15 @@ from io import StringIO
 from unittest.mock import patch
 from console import HBNBCommand
 
+
 class TestHBNBCommandCreate(unittest.TestCase):
     """Test cases for the create command with parameters in HBNBCommand"""
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_create_with_params(self, mock_stdout):
         """Test create command with parameters"""
-        with patch('sys.stdin', StringIO('create User name="John_Doe" age=30 height=6.2\n')):
+        with patch('sys.stdin', StringIO('create User name="John_Doe" \
+                                         age=30 height=6.2\n')):
             HBNBCommand().cmdloop()
         output = mock_stdout.getvalue().strip()
         self.assertTrue(len(output) > 0)  # Ensuring an ID is printed
@@ -19,10 +21,12 @@ class TestHBNBCommandCreate(unittest.TestCase):
     @patch('sys.stdout', new_callable=StringIO)
     def test_create_with_invalid_params(self, mock_stdout):
         """Test create command with invalid parameters"""
-        with patch('sys.stdin', StringIO('create User name="John_Doe" invalid_param="invalid"\n')):
+        with patch('sys.stdin', StringIO('create User name="John_Doe" \
+                                         invalid_param="invalid"\n')):
             HBNBCommand().cmdloop()
         output = mock_stdout.getvalue().strip()
         self.assertTrue(len(output) > 0)  # Ensuring an ID is printed
+
 
 if __name__ == '__main__':
     unittest.main()
